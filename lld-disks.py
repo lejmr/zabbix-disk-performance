d09be0ba15048541e9ff6a74ec20684a2f614f6a
Miloš Kozák <milos.kozak@lejmr.com>
2019-06-30 21:52:57 +0200

    Improved filtering

--- lld-disks.py
+++ lld-disks.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import os
 import json
+import re
+
 
 if __name__ == "__main__":
     # Iterate over all block devices, but ignore them if they are in the
     # skippable set
-    skippable = ("sr", "loop", "ram")
+    skippable = (r'^(sr|loop|ram|fd|dm)', r'^(sd\w|hd\w|nvme\d+n\d+p|xvd\w|vd\w)\d+$')
     devices = (device for device in os.listdir("/sys/class/block")
-               if not any(ignore in device for ignore in skippable))
+               if not any(re.search(ignore, device) for ignore in skippable))
     data = [{"{#DEVICENAME}": device} for device in devices]
     print(json.dumps({"data": data}, indent=4))
